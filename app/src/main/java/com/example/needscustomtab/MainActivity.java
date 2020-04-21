15ad1e95d8ef6c83088c3a4557b55756bc4dfe69
Chamara Wijerathna <pathmakumaraad@gmail.com>
2020-04-21 14:10:01 +0530

    launch as app and launch as website functionality, still points to kathlk.online

--- app/src/main/java/com/example/needscustomtab/MainActivity.java
+++ app/src/main/java/com/example/needscustomtab/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.needscustomtab;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.browser.customtabs.CustomTabsIntent;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -16,14 +20,54 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.opn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openCustomTab("http://kathlk.online:1337/");
+                openCustomTab("kathlk.online:1337/");
+            }
+        });
+        findViewById(R.id.opnChrm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInChrome("kathlk.online:1337/");
             }
         });
     }
 
-    private  void  openCustomTab(String url){
-        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        CustomTabsIntent customTabsIntent = builder.build();
-        customTabsIntent.launchUrl(this, Uri.parse(url));
+    private void openCustomTab(String urlBase) {
+        boolean failed = false;
+        String mobileEndPoint = "mapp";
+        try {
+            String url = "http://" + urlBase + mobileEndPoint;
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            builder.setToolbarColor(Color.BLUE);
+            customTabsIntent.launchUrl(this, Uri.parse(url));
+        } catch (Exception e) {
+            // If chrome custom tabs got error, launch app with Chrome
+            openInChrome(urlBase);
+        }
+    }
+    private void openInChrome(String urlBase) {
+        try {
+            String mobileWebEndPoint = "mweb";
+            String url = "http://" + urlBase + mobileWebEndPoint;
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(browserIntent);
+        } catch (Exception e) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Error")
+                    .setMessage("Please check your internet connection. Contact administrator.")
+
+                    // Specifying a listener allows you to take an action before dismissing the dialog.
+                    // The dialog is automatically dismissed when a dialog button is clicked.
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Continue with delete operation
+                        }
+                    })
+
+                    // A null listener allows the button to dismiss the dialog and take no further action.
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
     }
 }
